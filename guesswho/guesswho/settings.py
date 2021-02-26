5fbcdd54924c2143afb70601e97843e0e7deeb97
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2021-02-26 19:45:17 +0100

    server psql config

--- guesswho/guesswho/settings.py
+++ guesswho/guesswho/settings.py
@@ -125,10 +125,23 @@ LOGOUT_REDIRECT_URL = 'home'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 if PRODUCTION:
-    #src: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Deployment
-    import dj_database_url
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
+    # Heroku specific
+    # #src: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Deployment
+    # import dj_database_url
+    # db_from_env = dj_database_url.config(conn_max_age=500)
+    # DATABASES['default'].update(db_from_env)
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'guesswho',                      # Or path to database file if using sqlite3.
+            # The following settings are not used with sqlite3:
+            'USER': 'guesswho',
+            'PASSWORD': 'ENTER_PASSWORD_HERE',
+            'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+            'PORT': '',                      # Set to empty string for default.
+        }
+    }
 
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
