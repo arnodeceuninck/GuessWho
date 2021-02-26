1a54d19e9aa4518458b9510cc2c903e11738ef86
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2021-02-26 16:39:52 +0100

    updated heroku url

--- guesswho/guesswho/settings.py
+++ guesswho/guesswho/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1", "https://custom-guesswho.herokuapp.com/"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1", "custom-guesswho.herokuapp.com"]
 
 # Application definition
 
